fa942a72c3bca2e968feed084c3d73b81acf989b
朱乾锋 <zhuqianfeng@aidcloud.cn>
2023-08-02 17:59:41 +0800

    #

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker run -d -p 3000:3000 \
-   -e OPENAI_API_KEY="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" \
+   -e OPENAI_API_KEY="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" \
    -e HIDE_USER_API_KEY=1 \
    -e DISABLE_GPT4=1 \
    --restart=unless-stopped \
