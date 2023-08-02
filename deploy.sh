400706b25487c33c75051c0693a54ac893817221
朱乾锋 <zhuqianfeng@aidcloud.cn>
2023-08-02 17:35:46 +0800

    #

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ docker run -d -p 3000:3000 \
    -e OPENAI_API_KEY="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" \
    -e HIDE_USER_API_KEY=1 \
    -e DISABLE_GPT4=1 \
-   --restart=unless-stopped
+   --restart=unless-stopped \
    yidadaa/chatgpt-next-web
