0db898c0dfc4ee75e6249bcde50f550bb601c20e
朱乾锋 <zhuqianfeng@aidcloud.cn>
2023-11-27 16:46:04 +0800

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
-import { type Metadata } from 'next';
+import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "知汇科技-AI平台",
-  description: "聊天机器人",
+  title: "ChatGPT Next Web",
+  description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "知汇科技-AI平台",
+    title: "ChatGPT Next Web",
     statusBarStyle: "default",
   },
 };
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker run -d -p 3000:3000 \
-   -e OPENAI_API_KEY="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" \
-   -e HIDE_USER_API_KEY=1 \
-   -e DISABLE_GPT4=1 \
-   --restart=unless-stopped \
-   yidadaa/chatgpt-next-web
